a3c61eb6ebe30416083a2c38e1b923035fdbd37a
Roman <webdeveloverroman@gmail.com>
2025-10-01 12:19:21 +0300

    Delete unused routes

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -41,48 +41,4 @@ class PostRepository
 
         return $posts;
     }
-
-    public function getPost(int $id): ?Post
-    {
-        $posts = $this->getPosts();
-        return $posts[$id] ?? null;
-    }
-
-    public function lastId(): int
-    {
-        $posts = $this->getPosts();
-        if (empty($posts)) {
-            return 0;
-        }
-
-        return max(array_keys($posts));
-    }
-
-    public function addPost(Post $post): int
-    {
-        $posts = $this->getPosts();
-        $id = $post->getId();
-        $posts[$id] = $post;
-        $this->savePosts($posts);
-        return $id;
-    }
-
-    public function removePost(int $id): bool
-    {
-        $posts = $this->getPosts();
-        if (!isset($posts[$id])){
-            return false;
-        }
-        unset($posts[$id]);
-        $this->savePosts($posts);
-        return true;
-    }
-
-    private function savePosts(array $posts): void
-    {
-        $res = file_put_contents($this->storage, json_encode($posts, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
-        if ($res === false) {
-            throw new \RuntimeException('Cannot write to DB file.');
-        }
-    }
 }
