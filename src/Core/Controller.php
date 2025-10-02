396548dc4c5a23f0def587131201985bba63be49
Roman <webdeveloverroman@gmail.com>
2025-10-02 19:25:47 +0300

    Change render

--- src/Core/Controller.php
+++ src/Core/Controller.php
@@ -4,22 +4,21 @@ declare(strict_types=1);
 namespace App\Core;
 
 use App\Repository\PostRepository;
+use Twig\Environment;
 
 abstract class Controller
 {
     public function __construct(
         protected array $config,
-        protected PostRepository $repo
+        protected PostRepository $repo,
+        protected Environment $twig
     ) {}
 
     protected function render(string $template, array $data): void
     {
-        extract($data, EXTR_SKIP);
-        ob_start();
-
-        require $this->config['paths']['view'] . '/' . $template . '.php';
-        $content = ob_get_clean();
-
-        require $this->config['paths']['view'] . '/layout.php';
+        $content = $this->twig->render($template . '.twig', $data);
+        echo $this->twig->render('layout.twig', [
+            'content' => $content,
+        ]);
     }
 }
