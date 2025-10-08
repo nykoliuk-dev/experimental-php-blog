3623afa158d99280070a094baf0a83b71c155139
Roman <webdeveloverroman@gmail.com>
2025-10-08 20:56:42 +0300

    Edit PostController

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Core\Controller;
 use App\Repository\PostRepository;
+use Rakit\Validation\Validator;
 
 class PostController extends Controller
 {
@@ -21,15 +22,17 @@ class PostController extends Controller
     {
         return 'один пост';
     }
-    public function create(): string
+    public function create(): void
     {
         $this->render('posts/create', [
             'title' => 'Добавить статью',
         ]);
-        return 'форма создания';
     }
-    public function store(): string
+    public function store(Validator $validator): string
     {
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
         return 'Добавить пост';
     }
 }
