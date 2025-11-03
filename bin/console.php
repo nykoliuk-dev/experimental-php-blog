10598dc8a7f45936766dcc1aa0df841d40ddd294
Roman <webdeveloverroman@gmail.com>
2025-11-03 13:07:46 +0200

    Add console and migrate command

--- bin/console.php
+++ bin/console.php
@@ -1,2 +1,29 @@
 <?php
 declare(strict_types=1);
+
+use App\Command\MigrateCommand;
+use App\Repository\DatabasePostRepository;
+use App\Repository\JsonPostRepository;
+use App\Service\DatabaseService;
+use Symfony\Component\Console\Application;
+
+$bootstrap = require_once dirname(__DIR__) . '/bootstrap/init.php';
+$config = $bootstrap['config'];
+
+$dsn = $config->getDsn();
+$user = $config->env['DB_USER'];
+$pass = $config->env['DB_PASS'];
+
+$pdo = new PDO($dsn, $user, $pass, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+]);
+
+$dbServ = new DatabaseService($pdo);
+
+$jsonRepo = new JsonPostRepository($config->dbPath());
+$dbRepo   = new DatabasePostRepository($dbServ);
+
+$app = new Application('Basic Blog CLI', '1.0.0');
+$app->add(new MigrateCommand($jsonRepo, $dbRepo));
+$app->run();
--- src/Command/MigrateCommand.php
+++ src/Command/MigrateCommand.php
@@ -1,2 +1,76 @@
 <?php
 declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Model\Post;
+use App\Repository\DatabasePostRepository;
+use App\Repository\JsonPostRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * CLI command to migrate posts from JSON storage to the SQL database.
+ */
+class MigrateCommand extends Command
+{
+    /**
+     * Command name in console
+     */
+    protected static $defaultName = 'app:migrate:posts';
+
+    public function __construct(
+        private JsonPostRepository $jsonRepo,
+        private DatabasePostRepository $dbRepo
+    )
+    {
+        parent::__construct('app:migrate:posts');
+    }
+
+
+    /**
+     * Command description
+     */
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Migrate posts from JSON storage to the SQL database.');
+    }
+
+    /**
+     * Executing a command
+     *
+     * @param InputInterface $input Arguments and options
+     * @param OutputInterface $output Console output
+     * @return int Command exit code (0 = success)
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln('<info>Starting migration...</info>');
+        $posts = $this->jsonRepo->getPosts();
+
+        $count = 0;
+        foreach ($posts as $post){
+            if($this->postReadyToMigrate($post)){
+                $this->dbRepo->addPost($post);
+                $count++;
+            }else{
+                $output->writeln("<error>Post №{$post->getId()} is not valid</error>");
+            }
+        }
+
+        $output->writeln("$count posts successfully added");
+
+        return Command::SUCCESS;
+    }
+
+    private function postReadyToMigrate(Post $post): bool
+    {
+        if ($post->getTitle() === '' || $post->getContent() === '') {
+            return false;
+        }
+        return true;
+    }
+}
