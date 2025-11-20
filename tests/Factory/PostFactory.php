e80f4a5b2da4d3a9edb97370e56fe2a41ca4567b
Roman <webdeveloverroman@gmail.com>
2025-11-20 13:33:07 +0200

    Add PostFactory

---
+++ tests/Factory/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Factory;
+
+use App\Model\Post;
+
+class PostFactory
+{
+    public static function create(
+        ?int $id = null,
+        ?int $userId = null,
+        string $date = '2025-11-07',
+        string $title = 'Title',
+        string $slug = 'title',
+        string $content = 'Content',
+        string $imageName = 'img.jpg'
+    ): Post {
+        return new Post(
+            id: $id,
+            userId: $userId,
+            date: $date,
+            title: $title,
+            slug: $slug,
+            content: $content,
+            imageName: $imageName
+        );
+    }
+}
