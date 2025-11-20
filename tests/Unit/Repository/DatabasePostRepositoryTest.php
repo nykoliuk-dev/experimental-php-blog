fa5e83e228ba38bb591a4cfac12e48692a74e4cb
Roman <webdeveloverroman@gmail.com>
2025-11-20 13:43:20 +0200

    Use PostFactory

--- tests/Unit/Repository/DatabasePostRepositoryTest.php
+++ tests/Unit/Repository/DatabasePostRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Unit\Repository;
 
 use App\Model\Post;
 use App\Repository\DatabasePostRepository;
+use Tests\Factory\PostFactory;
 use PDO;
 use PHPUnit\Framework\TestCase;
 
@@ -20,15 +21,17 @@ class DatabasePostRepositoryTest extends TestCase
         $this->pdo->exec("
             CREATE TABLE posts (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
                 date TEXT NOT NULL,
                 title TEXT NOT NULL,
+                slug TEXT NOT NULL,
                 content TEXT NOT NULL,
                 image_name TEXT NOT NULL
             )
         ");
 
         $dbService = new \App\Service\DatabaseService($this->pdo);
-        $this->repo = new \App\Repository\DatabasePostRepository($dbService);
+        $this->repo = new DatabasePostRepository($dbService);
     }
 
     protected function tearDown(): void
@@ -39,7 +42,7 @@ class DatabasePostRepositoryTest extends TestCase
 
     public function testAddPostSavesAndReturnsConsistentData(): void
     {
-        $post = new Post(null, '2025-11-07', 'Title', 'Content', 'img.jpg');
+        $post = PostFactory::create();
 
         $id = $this->repo->addPost($post);
         $actualPost = $this->repo->getPost($id);
@@ -55,7 +58,7 @@ class DatabasePostRepositoryTest extends TestCase
 
     public function testRemovePost(): void
     {
-        $post = new Post(null, '2025-11-07', 'Title', 'Content', 'img.jpg');
+        $post = PostFactory::create();
 
         $id = $this->repo->addPost($post);
 
@@ -71,8 +74,8 @@ class DatabasePostRepositoryTest extends TestCase
 
     public function testGetPostsReturnsAllSavedPosts(): void
     {
-        $post1 = new Post(null, '2025-11-07', 'Title', 'Content', 'img.jpg');
-        $post2 = new Post(null, '2025-11-07', 'Title2', 'Content2', 'img.jpg');
+        $post1 = PostFactory::create();
+        $post2 = PostFactory::create(title: 'Title2', slug: 'title2', content: 'Content2');
 
         $firstId = $this->repo->addPost($post1);
         $secondId = $this->repo->addPost($post2);
@@ -80,8 +83,6 @@ class DatabasePostRepositoryTest extends TestCase
         $actualPosts = $this->repo->getPosts();
 
         $this->assertCount(2, $actualPosts);
-        $this->assertSame(1, $firstId);
-        $this->assertSame(2, $secondId);
 
         $this->assertPostsEqual($post1, $actualPosts[0]);
         $this->assertPostsEqual($post2, $actualPosts[1]);
@@ -89,11 +90,10 @@ class DatabasePostRepositoryTest extends TestCase
 
     public function testGetPostReturnsExactPostById(): void
     {
-        $post = new Post(null, '2025-11-07', 'Title', 'Content', 'img.jpg');
+        $post = PostFactory::create();
         $expectedId = 1;
 
         $actualId = $this->repo->addPost($post);
-
         $actualPost = $this->repo->getPost($actualId);
 
         $this->assertSame($expectedId, $actualId);
@@ -102,8 +102,10 @@ class DatabasePostRepositoryTest extends TestCase
 
     private function assertPostsEqual(Post $expected, Post $actual): void
     {
+        $this->assertSame($expected->getUserId(), $actual->getUserId());
         $this->assertSame($expected->getDate(), $actual->getDate());
         $this->assertSame($expected->getTitle(), $actual->getTitle());
+        $this->assertSame($expected->getSlug(), $actual->getSlug());
         $this->assertSame($expected->getContent(), $actual->getContent());
         $this->assertSame($expected->getImgName(), $actual->getImgName());
     }
