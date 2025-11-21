78c86c16ab64f511106c5fbecd1397017c48bbb6
Roman <webdeveloverroman@gmail.com>
2025-11-21 15:44:42 +0200

    Update PostFactory. Add method to create Post without slug.

--- tests/Factory/PostFactory.php
+++ tests/Factory/PostFactory.php
@@ -26,4 +26,41 @@ class PostFactory
             imageName: $imageName
         );
     }
+
+    public static function createWithoutSlug(
+        ?int $id = null,
+        ?int $userId = null,
+        string $date = '2025-11-07',
+        string $title = 'Title',
+        string $content = 'Content',
+        string $imageName = 'img.jpg'
+    ): Post
+    {
+        $slug = null;
+
+        return self::createPostUnsafely([
+            'id' => $id,
+            'userId' => $userId,
+            'date' => $date,
+            'title' => $title,
+            'slug' => $slug,
+            'content' => $content,
+            'imageName' => $imageName
+        ]);
+    }
+
+    private static function createPostUnsafely(array $data): Post
+    {
+        $reflection = new \ReflectionClass(Post::class);
+
+        $post = $reflection->newInstanceWithoutConstructor();
+
+        foreach ($data as $propertyName => $value){
+            $property = $reflection->getProperty($propertyName);
+            $property->setAccessible(true);
+            $property->setValue($post, $value);
+        }
+
+        return $post;
+    }
 }
