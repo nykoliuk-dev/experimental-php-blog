9f6a7ad35c5b1c4b71e47a55e436bcdb22815cee
Roman <webdeveloverroman@gmail.com>
2025-11-22 22:49:31 +0200

    Add logout action

--- src/Controller/Auth/LogoutController.php
+++ src/Controller/Auth/LogoutController.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 namespace App\Controller\Auth;
 
 use App\Core\Controller;
+use App\Service\AuthService;
 
 class LogoutController extends Controller
 {
+    public function logout(AuthService $authService): void
+    {
+        $authService->logout();
 
+        header("Location: /");
+        exit;
+    }
 }
