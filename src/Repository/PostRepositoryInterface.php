452c4391bc59438f08746c22d8d378dd92214aa1
Roman <webdeveloverroman@gmail.com>
2025-11-24 06:11:38 +0200

    Add getPostTags, getPostCategories contracts

--- src/Repository/PostRepositoryInterface.php
+++ src/Repository/PostRepositoryInterface.php
@@ -16,7 +16,13 @@ interface PostRepositoryInterface
 
     public function removePost(int $id): bool;
 
-    public function setTags(int $postId, array $tagIds): void;
+    public function setPostTags(int $postId, array $tagIds): void;
 
-    public function setCategories(int $postId, array $categoryIds): void;
+    public function setPostCategories(int $postId, array $categoryIds): void;
+
+    /** @return int[]  Array of tag IDs */
+    public function getPostTags(int $postId): array;
+
+    /** @return int[]  Array of category IDs */
+    public function getPostCategories(int $postId): array;
 }
