efd79c14125011e0aa8ba0c2929ede495d93f769
Roman <webdeveloverroman@gmail.com>
2025-11-25 18:52:58 +0200

    Add TransactionManager

---
+++ src/Service/TransactionManager.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+class TransactionManager
+{
+    public function __construct(private \PDO $pdo) {}
+
+    public function wrap(callable $callback)
+    {
+        try {
+            $this->pdo->beginTransaction();
+            $result = $callback();
+            $this->pdo->commit();
+            return $result;
+
+        } catch (\Throwable $e) {
+            $this->pdo->rollBack();
+            throw $e;
+        }
+    }
+
+    public function beginTransaction(): void
+    {
+        $this->pdo->beginTransaction();
+    }
+
+    public function commit(): void
+    {
+        $this->pdo->commit();
+    }
+
+    public function rollBack(): void
+    {
+        if ($this->pdo->inTransaction()) {
+            $this->pdo->rollBack();
+        }
+    }
+
+    public function inTransaction(): bool
+    {
+        return $this->pdo->inTransaction();
+    }
+}
