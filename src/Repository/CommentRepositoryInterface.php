4e1a9a7a9b14eec31a09dd8bef959a7dd156338d
Roman <webdeveloverroman@gmail.com>
2025-11-25 06:03:31 +0200

    Add Pagination dependency

--- src/Repository/CommentRepositoryInterface.php
+++ src/Repository/CommentRepositoryInterface.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Model\Comment;
+use App\ValueObject\Pagination;
 
 interface CommentRepositoryInterface
 {
     /** @return Comment[] */
-    public function getCommentsByPost(int $postId): array;
+    public function getCommentsByPost(int $postId, Pagination $pagination): array;
 
     public function getComment(int $id): ?Comment;
 
