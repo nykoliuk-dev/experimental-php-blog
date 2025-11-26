2225bc299e18017c3b5b96000fb5083f9562e734
Roman <webdeveloverroman@gmail.com>
2025-11-26 14:37:09 +0200

    Add CommentService

---
+++ src/Service/CommentService.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Model\Comment;
+use App\Model\ValueObject\CommentId;
+use App\Model\ValueObject\PostId;
+use App\Model\ValueObject\UserId;
+use App\Repository\CommentRepositoryInterface;
+
+class CommentService
+{
+    public function __construct(
+        private CommentRepositoryInterface $repo,
+    )
+    {
+    }
+
+    public function createComment(PostId $postId, ?UserId $userId, string $content): CommentId
+    {
+        return $this->repo->addComment(new Comment(
+            id: null,
+            postId: $postId,
+            userId: $userId,
+            content: $content,
+            createdAt: date('Y-m-d H:i:s'),
+        ));
+    }
+}
