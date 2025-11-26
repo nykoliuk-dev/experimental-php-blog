fbdee4df34424747b62f19055cdddc95130c1fad
Roman <webdeveloverroman@gmail.com>
2025-11-26 15:53:10 +0200

    Add SessionUserProvider

---
+++ src/Service/SessionUserProvider.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Model\ValueObject\UserId;
+
+class SessionUserProvider implements CurrentUserProviderInterface
+{
+    public function getCurrentUserId(): ?UserId
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if (isset($_SESSION['user']['id'])) {
+            return new UserId((int)$_SESSION['user']['id']);
+        }
+        return null;
+    }
+}
