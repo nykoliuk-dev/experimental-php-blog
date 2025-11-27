98e32d27e5b79a6733f2702eda7f87a259e18a59
Roman <webdeveloverroman@gmail.com>
2025-11-27 07:51:58 +0200

    Add UserIdTest

---
+++ tests/Unit/Model/ValueObject/UserIdTest.php
@@ -0,0 +1,59 @@
+<?php
+declare(strict_types=1);
+
+namespace Model\ValueObject;
+
+use App\Model\ValueObject\UserId;
+use PHPUnit\Framework\TestCase;
+
+class UserIdTest extends TestCase
+{
+    public function testCreatesUserIdWithValidDataAndReturnCorrectValue(): void
+    {
+        $id = 1;
+        $userId = new UserId(1);
+        $this->assertSame($id, $userId->value());
+    }
+
+    /**
+     * @dataProvider invalidIntIdProvider
+     */
+    public function testThrowsExceptionWhenInvalidIntId(int  $id): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('UserId must be a positive integer.');
+        new UserId($id);
+    }
+
+    /**
+     * @dataProvider wrongTypeProvider
+     */
+    public function testThrowsExceptionWhenIdHasWrongType(mixed $id): void
+    {
+        $this->expectException(\TypeError::class);
+        new UserId($id);
+    }
+
+    public static function invalidIntIdProvider(): array
+    {
+        return [
+            'negative id' => [-5],
+            'zero id' => [0],
+        ];
+    }
+
+    public static function wrongTypeProvider(): array
+    {
+        return [
+            'float' => [1.5],
+            'string' => ['10'],
+            'string with letters' => ['4cats'],
+            'empty string' => [''],
+            'null' => [null],
+            'bool true' => [true],
+            'bool false' => [false],
+            'array' => [[]],
+            'object' => [(object)[]],
+        ];
+    }
+}
